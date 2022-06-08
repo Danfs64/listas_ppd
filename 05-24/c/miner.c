873cd31ca92524bf8900ea6301366896e3aafe60
Danfs <trabalhosdosilva@gmail.com>
2022-06-07 22:32:53 -0300

    O rpcgen gera um código q não compila :( Mesmo se declarar a struc no .h :((( Co-authored-by: PsicoThePato <PsicoThePato@users.noreply.github.com>

--- 05-24/c/miner.c
+++ 05-24/c/miner.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <openssl/sha.h>
+
 
 typedef struct {
     int challenge;
@@ -10,6 +13,17 @@ typedef struct {
 Transaction TRANSACTIONS[1000];
 int LAST_TRANS = -1;
 
+
+typedef struct {
+    int status;
+    char* seed;
+    int challenge;
+} TransactionInfo;
+
+/*
+    Gera o próximo challenge
+    O challenge aumenta a cada 2 chamadas
+*/
 int CHALL = 1;
 int generate_challenge() {
     CHALL++;
@@ -69,26 +83,60 @@ int getWinner(int tid) {
 }
 
 /*
-    Retorna uma tupla com:
+    Retorna uma struct que contêm:
     - o status;
     - a seed;
     - e o desafio.
     associados ao `tid`.
 
-    Se `tid` for inválido, retorna uma tupla (-1, None, -1)
+    Se `tid` for inválido, retorna a struct preenchida com (-1, NULL, -1)
 */
-// TODO C não tem tupla D:
-// int getSeed(int tid) {
-//     if(tid > LAST_TRANS)
-//         return (-1, None, -1);
+TransactionInfo getSeed(int tid) {
+    if(tid > LAST_TRANS) {
+        TransactionInfo ret;
+        ret.challenge = -1;
+        ret.status = -1;
+        ret.seed = NULL;
 
-//     Transaction transaction = TRANSACTIONS[tid];
-//     int challenge = transaction.challenge;
-//     char* seed = transaction.seed;
-//     int winner = transaction.winner;
+        return ret;
+    }
 
-//     return (getTransactionStatus(tid), seed, challenge);
-// }
+    Transaction transaction = TRANSACTIONS[tid];
+    TransactionInfo ret;
+
+    ret.challenge = transaction.challenge;
+    ret.status = (transaction.winner == -1);
+    ret.seed = transaction.seed;
+
+    return ret;
+}
+
+int check_seed(char* seed, int tid) {
+    if(tid > LAST_TRANS) return -1;
+    if(tid < LAST_TRANS) return  2;
+
+    int target_bits = TRANSACTIONS[LAST_TRANS].challenge;
+    int target_bytes = target_bits/8;
+    int remainder_bits = target_bits%8;
+
+    unsigned char sha_result[40];
+    SHA1(seed, strlen(seed), sha_result);
+
+    // Checks bits 8 by 8
+    for(int i = 0; i < target_bytes; i++) {
+        if((sha_result[i] & 0xFF) != 0) {
+            return 0;
+        }
+    }
+
+    // Creates a mask that has <target_bits> 1's and then enough 0's to complete a byte
+    int mask = ((1 << target_bits)-1) << (8 - target_bits);
+    // Checks the bits that can't be grouped in a byte
+    if((sha_result[target_bytes] & mask) != 0)
+        return 0;
+    else
+        return 1;
+}
 
 /*
     Submete uma semente (seed) para o hashing SHA-1 que
@@ -103,9 +151,12 @@ int submitChallenge(int tid, int cid, char* seed) {
     int result = check_seed(seed, tid);
 
     if(result == 1) {
+        // Finaliza a transação atual
         TRANSACTIONS[tid].winner = cid;
-        TRANSACTIONS[tid].seed = seed;
+        TRANSACTIONS[tid].seed = malloc(sizeof(char)*strlen(seed));
+        strcpy(TRANSACTIONS[tid].seed, seed);
 
+        // Gera a próxima transação
         LAST_TRANS++;
         TRANSACTIONS[LAST_TRANS].challenge = generate_challenge();
         TRANSACTIONS[LAST_TRANS].seed = NULL;
@@ -116,8 +167,27 @@ int submitChallenge(int tid, int cid, char* seed) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        /* code */
-    }
 
+    LAST_TRANS = 0;
+    TRANSACTIONS[0].challenge = generate_challenge();
+    TRANSACTIONS[0].winner = -1;
+    TRANSACTIONS[0].seed = NULL;
+
+    // int result = submitChallenge(0, 666, "qbcabcap");
+    // printf("%d\n", result);
+
+    // int tid = getTransactionID();
+    // printf("%d\n", tid);
+
+    // printf("%d %d\n", getChallenge(tid), getChallenge(tid-1));
+    // printf("%d %d\n", getTransactionStatus(tid), getTransactionStatus(tid-1));
+    // printf("%d %d\n", getWinner(tid), getWinner(tid-1));
+
+    // TransactionInfo seed1, seed2;
+    // seed1 = getSeed(tid);
+    // seed2 = getSeed(tid-1);
+    // printf("%d %s %d\n", seed1.challenge, seed1.seed, seed1.status);
+    // printf("%d %s %d\n", seed2.challenge, seed2.seed, seed2.status);
+
+    return 0;
 }
--- 05-24/c/miner.x
+++ 05-24/c/miner.x
@@ -1,9 +1,16 @@
 struct args {
-
+    int tid;
+    int cid;
+    char* seed;
 };
 
 program PROG {
     version VERSAO {
-
+        int GETTRANSACTIONID(args) = 1;
+        int GETCHALLENGE(args) = 2;
+        int GETTRANSACTIONSTATUS(args) = 3;
+        int GETWINNER(args) = 4;
+        TransactionInfo GETSEED(args) = 5;
+        int SUBMITCHALLENGE(args) = 6;
     } = 100;
 } = 5555;
