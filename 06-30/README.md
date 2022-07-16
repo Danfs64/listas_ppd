eb1b0d678efc1e8dae639930294c53f3443106af
Danfs <trabalhosdosilva@gmail.com>
2022-07-16 01:27:34 -0300

    x

--- 06-30/README.md
+++ 06-30/README.md
@@ -25,17 +25,31 @@ Todas as filas usadas são:
 
 As exchanges usadas são:
 
-* ppd/check-in: Exchange em que os clientes mandam mensagens para mostrar que estão conectados
+* `ppd/check-in`: Exchange em que os clientes mandam mensagens para mostrar que estão conectados
   * Campos do body:
     * `pid`: o ID específico do cliente que está mandando a mensagem
-* ppd/voting: Exchange em que os clientes mandam os seus votos para decidir o líder
+* `ppd/leader`: Exchange em que os clientes mandam os seus votos para decidir o líder
   * Campos do body:
     * `sender`: o ID do cliente que está votando
     * `vote`: o ID do cliente que está sendo votado por `sender`
-* ppd/challenges: Exchange em que o líder envia os challenges para todos os clientes
+* `ppd/challenges`: Exchange em que o líder envia os challenges para todos os clientes
   * Campos do body:
+    * `cid`: o ID do challenge, iniciado em 0 e incrementado a cada challenge gerado
     * `sender`: o ID do rementente da mensagem, idealmente o líder
     * `challenge`: Dificuldade do desafio a ser resolvido pelos clientes
+* `ppd/solutions`: Exchange em que os clientes enviam suas propostas de resolução do desafio
+  * Campos do body:
+    * `cid`: o ID do challenge a ser resolvido
+    * `sender`: o ID de quem enviou a proposta de solução
+    * `solution`: A seed que supostamente resolve o challenge
+* `ppd/voting`: Exchange em que são mandadas as mensagens de votação das soluções
+  * Campos do body:
+    * `solved`: Booleano que identifica se o emissor do voto concorda que o desafio foi solucionado
+
+P.S.: Na exchange de votação, como estamos supondo que:
+
+* Não há agentes maliciosos: não precisamos identificar quem está enviando cada voto, pois cada um só manda um voto
+* Não há falhas de comunicação: sempre haverá no máximo uma votação em andamento (os clientes esperam todos os votos serem enviados antes de voltarem a olhar a fila de solutions), não sendo necessário identificar o desafio tentando ser resolvido, pois cada `NUM_CLIENTS` votos seguidos definem uma votação.
 
 ## Fluxo de funcionamento
 
@@ -43,17 +57,21 @@ As exchanges usadas são:
 
 * Somente inicializa as filas que serão usadas
 
-### Clientes
+### Cliente
 
 * Faz o próprio check-in, mandando na fila `check-in` o seu PID (Personal ID)
 * Fica ouvindo a fila `check-in` até reconhecer que todos os outros membros entraram
-  * Sempre que ele percebe um cliente novo, ele re-envia seu check-in, para garantir que esse cliente novo receba
+  * Sempre que percebe um cliente novo, re-envia seu check-in, para garantir que esse cliente novo receba
 * Após todos os membros estarem dentro, faz-se a votação do líder
-  * Cada cliente vota aleatoriamente e manda seu voto na fila `voting`
+  * Cada cliente vota aleatoriamente e manda seu voto na fila `leader`
   * O vencedor da votação é o cliente com mais votos, usando o PID como desempate
     * O menor PID vence o desempate
 * Feita a votação para líder, o líder irá gerar um desafio aleatório e mandar na fila `challenge`
-* Os clientes então consumiram o challenge da fila `challenge` e começarão a tentar resolvê-lo
-* O processo de resolução do desafio depende da dificuldade dele
+* Consome o challenge da fila `challenge` e começa a tentar resolvê-lo em uma thread/processo separado
+  * A thread/processo separado manda para a fila `solution` sempre que encontra uma solução
+* O processo de resolução do desafio depende da dificuldade:
   * Desafios de dificuldade até 20 são bastante rápidos, então não se usa paralelismo para resolvê-los
   * Desafios de dificuldade maior do que 20 serão paralelizados
+* Consome da fila de `solution` para checar quando um desafio foi resolvido
+* Para cada resolução lida de `solution`, o cliente checa a solução e manda seu voto para a fila de `voting`
+* Se a votação passa, o processo/thread de resolução é parado, a tabela interna de transações é atualizada, e volta para a consumir challenges para serem resolvidos
--- 06-30/client.py
+++ 06-30/client.py
@@ -1,13 +1,29 @@
+from dataclasses import dataclass
 from random import randint
-import pika
+from hashlib import sha1
+from time import sleep
 import json
+import pika
 
 NUM_CLIENTS = 3
 NUM_PROCESSES = 6
 MAX_SEED = 2147483647
 
+@dataclass
+class Transaction:
+    challenge: int
+    seed: int
+    winner: int
+
+    def __str__(self) -> str:
+        return (
+            f"Challenge: {self.challenge} | "
+            f"Seed vencedora: {self.seed if self.seed else 'Nenhuma'} | "
+            f"Cliente vencedor: {self.winner if (self.winner != -1) else 'Nenhum'}"
+        )
+
+TRANSACTIONS: dict[int, Transaction] = {}
 MY_PID = randint(0, 1<<32)
-TRANSACTIONS: list[int] = []
 
 CONNECTION = pika.BlockingConnection(
     pika.ConnectionParameters(host="localhost", heartbeat=600)
@@ -66,110 +82,170 @@ def minerar(challenge: int) -> int:
                         break
     return solution
 
+# FAZER CHECKIN NO BROKER
+def check_in() -> None:
+    CHANNEL.basic_publish(
+        exchange='ppd/check-in',
+        routing_key='',
+        body=json.dumps({"pid": MY_PID})
+    )
+    print(f"Eu sou o cliente {MY_PID} e eu fiz meu check-in")
+
+# ESPERAR TODOS OS MEMBROS ENTRAREM
+def wait_others(queue: str) -> set[int]:
+    clients = {MY_PID}
+    for _, _, body in CHANNEL.consume(queue):
+        new_client = int(json.loads(body)['pid'])
+        print(f"Cliente {new_client} fez check-in")
+
+        if new_client not in clients:
+            print(f"Cliente {new_cliente} é um cliente novo")
+            clients.add(new_client)
+            # Reenvia o check-in sempre que detecta um cliente novo
+            CHANNEL.basic_publish(
+                exchange='ppd/check-in',
+                routing_key='',
+                body=json.dumps({"pid": MY_PID})
+            )
+            print(f"Clientes conhecidos ({len(clientes)}): {clients}")
+            if len(clients) == NUM_CLIENTS:
+                break
+    CHANNEL.cancel()
+    return clients
+
+# VOTAR NO LÍDER
+def leader_vote(clients: set) -> None:
+    my_vote = list(clients)[randint(0, NUM_CLIENTS-1)]
+    CHANNEL.basic_publish(
+        exchange='ppd/voting',
+        routing_key='',
+        body=json.dumps({'sender': MY_PID, 'vote': my_vote})
+    )
+    print(f"Eu votei no {my_vote}")
+
+# RESULTADO DA VOTAÇÃO
+def leader_result(clients: set, queue: str) -> int:
+    votes = {pid: 0 for pid in clients}
+    counter = 0
+    for _, _, body in CHANNEL.consume(queue):
+        body = json.loads(body)
+        sender = int(body['sender'])
+        voted_client = int(body['vote'])
+        if sender not in clients:
+            print((
+                "PANIC!\n"
+                f"Voto de cliente desconhecido ({sender}) recebido!\n"
+                "Ignorando voto"
+            ))
+        else:
+            votes[voted_client] += 1
+            counter += 1
+            if counter == NUM_CLIENTS:
+                break
+    CHANNEL.cancel()
+
+    print("RESULTADO DA VOTAÇÃO:")
+    print(votes)
+    most_votes = max(votes.values())
+    leader = [pid for pid, n in votes.items() if n == most_votes].sort()[0]
+    print(f"VENCEDOR: {leader}")
+    return leader
+
+# GERAÇÃO DE DESAFIO
+def publish_challenge(cid: int, x: int, y: int) -> None:
+    CHANNEL.queue_declare(queue="challenges")
+    challenge = randint(x, y)
+    CHANNEL.basic_publish(
+        exchange='ppd/challenges',
+        routing_key='',
+        body=json.dumps({'sender': MY_PID, 'cid': cid, 'challenge': challenge})
+    )
+    print(f"Publiquei o desafio {challenge}")
+
+# LEITURA E RESOLUÇÃO DO DESAFIO
+def publish_solution(cid: int, challenge: int) -> None:
+    # Entra em loop infinito tentando resolvr o challenge e mandando soluções
+    # Essa função (publish_solution) deve ser parada por um processo/thread a parte
+    while True:
+        solution = minerar(challenge)
+        CHANNEL.basic_publish(
+            exchange='ppd/solutions',
+            routing_key='',
+            body=json.dumps({'sender': MY_PID, 'cid': cid, 'solution': solution})
+        )
+
+# PROCESSAR SOLUÇÕES
+def process_solutions(challenge_id: int, solution_queue: str, voting_queue: str) -> None:
+    for _, _, body in CHANNEL.consume(solution_queue):
+        body = json.loads(json)
+        cid = int(json['cid'])
+        sender = int(json['sender'])
+        solution = int(json['solution'])
+
+        # Se é uma tentativa de solução do challenge correto
+        if cid == challenge_id:
+            solved = check_seed(solution, TRANSACTIONS[cid].challenge)
+            CHANNEL.basic_publish(
+                exchange='ppd/solution_voting',
+                routing_key='',
+                body=json.dumps({'solved': solved})
+            )
+            voting_result = process_voting(voting_queue)
+            # Se a votação aceita a resposta, registra e retorna
+            if voting_result:
+                TRANSACTIONS[cid].seed = solution
+                TRANSACTIONS[cid].winner = sender
+                return
+
+# RESULTADO VOTAÇÃO
+def process_voting(queue: str) -> bool:
+    votes = {True: 0, False: 0}
+    counter = 0
+    for _, _, body in CHANNEL.consume(queue):
+        solved = bool(body['solved'])
+        votes[solved] += 1
+        counter += 1
+        if counter == NUM_CLIENTS:
+            break
+
+    return votes[True] > votes[False]
 
 if __name__ == "__main__":
-
-    # FAZER CHECKIN NO BROKER
-    def check_in():
-        CHANNEL.basic_publish(
-            exchange='ppd/check-in',
-            routing_key='',
-            body=json.dumps({"pid": MY_PID})
-        )
-        print(f"Eu sou o cliente {MY_PID} e eu fiz meu check-in")
-
-    # ESPERAR TODOS OS MEMBROS ENTRAREM
-    def wait_others(queue: str):
-        clients = {MY_PID}
-        for _, _, body in CHANNEL.consume(queue):
-            new_client = int(json.loads(body)['pid'])
-            print(f"Cliente {new_client} fez check-in")
-
-            if new_client not in clients:
-                print(f"Cliente {new_cliente} é um cliente novo")
-                clients.add(new_client)
-                # Reenvia o check-in sempre que detecta um cliente novo
-                CHANNEL.basic_publish(
-                    exchange='ppd/check-in',
-                    routing_key='',
-                    body=json.dumps({"pid": MY_PID})
-                )
-                print(f"Clientes conhecidos ({len(clientes)}): {clients}")
-                if len(clients) == NUM_CLIENTS:
-                    break
-        CHANNEL.cancel()
-        return clients
-
-    # VOTAR NO LÍDER
-    def vote(clients: set):
-        my_vote = list(clients)[randint(0, NUM_CLIENTS-1)]
-        CHANNEL.basic_publish(
-            exchange='ppd/voting',
-            routing_key='',
-            body=json.dumps({'sender': MY_PID, 'vote': my_vote})
-        )
-        print(f"Eu votei no {my_vote}")
-
-    # RESULTADO DA VOTAÇÃO
-    def voting_result(clients: set, queue: str):
-        votes = {pid: 0 for pid in clients}
-        counter = 0
-        for _, _, body in CHANNEL.consume(queue):
-            body = json.loads(body)
-            sender = int(body['sender'])
-            voted_client = int(body['vote'])
-            if sender not in clients:
-                print((
-                    "PANIC!\n"
-                    f"Voto de cliente desconhecido ({sender}) recebido!\n"
-                    "Ignorando voto"
-                ))
-            else:
-                votes[voted_client] += 1
-                counter += 1
-                if counter == NUM_CLIENTS:
-                    break
-        CHANNEL.cancel()
-
-        print("RESULTADO DA VOTAÇÃO:")
-        print(votes)
-        most_votes = max(votes.values())
-        leader = [pid for pid, n in votes.items() if n == most_votes].sort()[0]
-        print(f"VENCEDOR: {leader}")
-        return leader
-
-    # GERAÇÃO DE DESAFIO
-    def publish_challenge(cid: int, x: int, y: int):
-        CHANNEL.queue_declare(queue="challenges")
-        challenge = randint(x, y)
-        CHANNEL.basic_publish(
-            exchange='ppd/challenges',
-            routing_key='',
-            body=json.dumps({'sender': MY_PID, 'cid': cid, 'challenge': challenge})
-        )
-        print(f"Publiquei o desafio {challenge}")
-
-    # LEITURA E RESOLUÇÃO DO DESAFIO
-
-    # PROCESSAR DESAFIO ACEITO
-
-    # Declaração das filas usadas
+    # Declaração das exchanges/filas usadas
     check_in_queue = set_exchange(CHANNEL, 'ppd/check-in', 'fanout')
-    voting_queue = set_exchange(CHANNEL, 'ppd/voting', 'fanout')
+    leader_queue = set_exchange(CHANNEL, 'ppd/leader', 'fanout')
     challenge_queue = set_exchange(CHANNEL, 'ppd/challenges', 'fanout')
+    solution_queue = set_exchange(CHANNEL, 'ppd/solutions', 'fanout')
+    voting_queue = set_exchange(CHANNEL, 'ppd/voting', 'fanout')
 
     check_in()
     clients = wait_others(check_in_queue)
-    exit(0)
-    vote(clients)
-    leader = voting_result(clients, voting_queue)
+
+    leader_vote(clients)
+    leader = leader_result(clients, leader_queue)
 
     # A partir daqui, é o loop de:
     # - Líder gera challenge
     # - Clientes tentam resolver
     # - Uma resposta é aceita
     # - Volta pro início do loop
-    cid = 0
-    # while True:
-    if leader == MY_PID: publish_challenge(cid, 25, 25)
+    while True:
+        if leader == MY_PID: publish_challenge(len(TRANSACTIONS), 40, 40)
 
+        # Lê mensagens da fila de challenges até receber uma válida
+        for _, _, body in CHANNEL.consume(challenge_queue):
+            body = json.loads(body)
+            cid = int(body['cid'])
+            sender = int(body['sender'])
+            challenge = int(body['challenge'])
+            if cid == len(TRANSACTIONS) and sender == leader:
+                break
+
+        # Registra a transação a ser resolvida
+        TRANSACTIONS[cid] = Transaction(challenge, None, -1)
+
+        # Essa função deve ser executada por um processo/thread próprio
+        publish_solution(cid, challenge)
+        # Você deve parar a thread/processo executando `publish_solution`
+        # quando a função abaixo retornar
+        process_solutions(cid, solution_queue, voting_queue)
