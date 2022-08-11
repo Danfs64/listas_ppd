852e0f2b3860cee63b5bcb383c3b97365a87dd88
dev-machine <jmomcontato@gmail.com>
2022-08-11 02:03:25 -0300

    09/11 foi obra do rodrigo faro

---
+++ 07-26/rabbit_handler/handler.py
@@ -0,0 +1,32 @@
+from ..settings import *
+from ..domain import Queue
+from typing import Callable, Any, Collection
+
+def set_exchange(chann, exchange: str, exchange_type: str) -> str:
+    chann.exchange_declare(exchange=exchange, exchange_type=exchange_type)
+    queue = chann.queue_declare(queue='', exclusive=True)
+
+    queue_name = queue.method.queue
+    chann.queue_bind(exchange=exchange, queue=queue_name)
+    return queue_name
+
+def publish(exchange: Queue, body: str) -> None:
+    MANAGING_CHANN.publish(
+        exchange=exchange,
+        routing_key='',
+        body=body
+    )
+
+# gambiarra pq o sistema de tipos de python é um lixo
+blocking = type(MANAGING_CHANN)
+
+def get(get_func: Callable[[Any], (bool, Collection | int)], queue, args: list, collection=None):
+    body_gen = MANAGING_CHANN.consume(queue)
+   
+    _, _, body = body_gen.__next__()
+    loop_again = True
+    while loop_again:
+        loop_again, collection = get_func(body, collection, *args)
+        _, _, body = body_gen.__next__()
+    MANAGING_CHANN.cancel()
+    return collection
